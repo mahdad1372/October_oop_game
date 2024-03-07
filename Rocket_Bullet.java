8170355b2af30eb5ac1e30ed92085b347e15805b
unknown <mnaderiniya@gmail.com>
2024-03-07 10:13:43 -0800

    adding the tank _rocket class

--- Rocket_Bullet.java
+++ Rocket_Bullet.java
@@ -1,6 +1,10 @@
+import java.awt.*;
+
 public class Rocket_Bullet extends Bullet{
-    public Rocket_Bullet(int bullet_x, int bullet_y) {
+    private Image Rocket_image;
+    public Rocket_Bullet(int bullet_x, int bullet_y , Image rocket_image) {
         super(bullet_x, bullet_y);
+        this.Rocket_image = rocket_image;
     }
 
     @Override
@@ -21,5 +25,11 @@ public class Rocket_Bullet extends Bullet{
     @Override
     public void setPosition_y(int y) {
         super.setPosition_y(y);
+    }
+    public Image getRocket_image(){
+        return  this.Rocket_image;
+    }
+    public void setRocket_image(Image rocket_img){
+        this.Rocket_image = rocket_img;
     }
 }
